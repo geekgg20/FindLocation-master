8e3983b02b5562fb92ea405e7f704df8f9530b06
geekgg20 <52476034+geekgg20@users.noreply.github.com>
2019-07-29 11:22:19 +0800

    fds

---
+++ src/com/github/heqiao2010/ResultVO.java
@@ -0,0 +1,98 @@
+package com.github.heqiao2010;
+
+/**
+ * 返回的地点信息封装
+ * @author heqiaoa
+ *
+ */
+public class ResultVO {
+	private String name;
+	private Point location;
+	private String uid;
+	private String city;
+	private String  district;
+	private String  business;
+	private String  cityid;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Point getLocation() {
+		return location;
+	}
+
+	public void setLocation(Point location) {
+		this.location = location;
+	}
+
+	public String getUid() {
+		return uid;
+	}
+
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getDistrict() {
+		return district;
+	}
+
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+
+	public String getBusiness() {
+		return business;
+	}
+
+	public void setBusiness(String business) {
+		this.business = business;
+	}
+
+	public String getCityid() {
+		return cityid;
+	}
+
+	public void setCityid(String cityid) {
+		this.cityid = cityid;
+	}
+
+	@Override
+	public String toString() {
+		return name;
+	}
+
+	public class Point{
+		private String lat;
+		private String lng;
+		public String getLat() {
+			return lat;
+		}
+		public void setLat(String lat) {
+			this.lat = lat;
+		}
+		public String getLng() {
+			return lng;
+		}
+		public void setLng(String lng) {
+			this.lng = lng;
+		}
+		@Override
+		public String toString() {
+			return lng + "," + lat;
+		}
+	};
+}
